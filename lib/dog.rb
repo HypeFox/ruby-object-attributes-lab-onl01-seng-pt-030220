e9030cd5e524186847e51f032e0636321d2a5647
Brandon Guffey <hypefox@gmail.com>
2020-03-16 23:43:05 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+# class Dog 
+#   def name=(dog_name)
+#     @name = dog_name
+#   end
+  
+#   def name
+#     @name
+#   end
+  
+#   def breed=(breed)
+#     @breed = breed
+#   end
+  
+#   def breed
+#     @breed
+#   end
+  
+# end
+
+class Dog 
+  attr_accessor :name
+  attr_accessor :breed
+end
